34d64a3d8fa73db784f4c3625d73f4d30cc7ab5f
GCorbel <guirec.corbel@gmail.com>
2015-08-01 14:47:20 -0400

    Add a password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   include Clearance::User
 
+  attr_accessor :password_confirmation
+
+  validates :email, confirmation: true
+  validates :password_confirmation, presence: true
+
   has_and_belongs_to_many :acquired_skills, class_name: 'Skill',
     join_table: :acquired_skills
   has_and_belongs_to_many :searched_skills, class_name: 'Skill',
