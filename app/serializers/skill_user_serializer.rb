910033b2c9fe915415cad8e316afc36ab2f661fd
GCorbel <guirec.corbel@gmail.com>
2015-08-10 08:10:10 -0400

    Add id of associations

--- app/serializers/skill_user_serializer.rb
+++ app/serializers/skill_user_serializer.rb
@@ -1,5 +1,5 @@
 class SkillUserSerializer < ActiveModel::Serializer
-  attributes :id, :description, :user, :skill, :kind
+  attributes :id, :description, :user, :skill, :kind, :user_id, :skill_id
 
   def user
     object.user_id
